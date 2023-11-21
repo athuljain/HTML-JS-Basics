491354044714c786b84bf9ec0ddfed2c0c351088
athuljain <athuljainkj2@gmail.com>
2023-11-21 11:51:03 +0530

    js functions examples

---
+++ SoftenJavaScript/AFunctions.js
@@ -0,0 +1,79 @@
+//1
+// Simple Function
+
+// function Wish(){
+//     console.log("hello.....");
+// }
+
+// Wish()
+
+
+//2
+
+// Function Declaration
+// function greet(name) {
+//   console.log("Hello, " + name + "!");
+// }
+
+// // Function Call
+// greet("John"); // Output: Hello, John!
+
+
+
+//3
+
+// // Function Expression
+// const add = function (a, b) {
+//     return a + b;
+//   };
+  
+//   // Function Call
+//   const result = add(3, 5);
+//   console.log(result); // Output: 8
+
+
+//4
+
+
+// // Arrow Function
+// const multiply = (x, y) => x * y;
+
+// // Function Call
+// const product = multiply(4, 6);
+// console.log(product); // Output: 24
+
+
+
+
+// Function with Rest Parameters
+// function sum(...numbers) {
+//     return numbers.reduce((total, num) => total + num, 0);
+//   }
+  
+//   // Function Call
+//   const total = sum(1, 2, 3, 4, 5);
+//   console.log(total); // Output: 15
+
+
+//IF Else Statements
+
+// if
+
+// let temperature = 35;
+
+// if (temperature > 30) {
+//   console.log("It's a hot day!");
+// }
+
+
+//else if
+
+// let temperature = 25;
+
+// if (temperature > 30) {
+//   console.log("It's a hot day!");
+// } else {
+//   console.log("It's not too hot today.");
+// }
+
+
